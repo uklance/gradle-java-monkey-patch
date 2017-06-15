2c7483c0f4575d87af00d52ee27fee6212890d1e
uklance <uklance@gmail.com>
2017-06-15 23:26:38 +0100

    Initial commit with sample patch and consumer projects

---
+++ java-monkey-patch/src/main/groovy/com/lazan/JavaMonkeyPatchPlugin.groovy
@@ -0,0 +1,99 @@
+package com.lazan
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ModuleVersionIdentifier
+import org.gradle.api.artifacts.ResolvedArtifact
+import org.gradle.api.artifacts.ResolvedDependency
+import org.gradle.api.file.FileCopyDetails
+import java.util.*
+
+
+class JavaMonkeyPatchPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.with {
+            apply plugin: 'java'
+            configurations {
+                monkeyPatchNonTransitive { transitive = false }
+                monkeyPatchTransitive
+            }
+            extensions.add('monkeyPatch', new MonkeyPatchExtension(project))
+            afterEvaluate {
+                boolean initialized = project.extensions.getByType(MonkeyPatchExtension).initialized
+            }
+
+        }
+    }
+
+    static class MonkeyPatchExtension {
+        private final Project project
+        private boolean initialized
+
+        MonkeyPatchExtension(Project project) {
+            this.project = project
+        }
+
+        void setTarget(Object target) {
+            if (initialized) {
+                throw new RuntimeException("Attempted to set monkey patch target twice")
+            }
+
+            project.with {
+                dependencies {
+                    monkeyPatchTransitive target
+                    monkeyPatchNonTransitive target
+                    compileOnly(target) {
+                        transitive = false
+                    }
+                }
+
+                Set<Map>  depSet = [] as Set
+                ResolvedDependency topDependency = configurations.monkeyPatchTransitive.resolvedConfiguration.firstLevelModuleDependencies.iterator().next()
+                topDependency.children.each { ResolvedDependency child ->
+                    child.allModuleArtifacts.each { ResolvedArtifact artifact ->
+                        ModuleVersionIdentifier mvi = artifact.moduleVersion.id
+                        def dependency = [
+                                group  : mvi.group,
+                                name   : mvi.name,
+                                version: mvi.version,
+                                ext    : artifact.extension
+                        ]
+                        if (artifact.classifier) {
+                            dependency['classifier'] = artifact.classifier
+                        }
+                        depSet << dependency
+                    }
+                }
+
+                depSet.each { Map dependency ->
+                    println "Adding $dependency"
+                    dependencies.compile(dependency) {
+                        transitive = false
+                    }
+                }
+
+                jar {
+                    with copySpec {
+                        from zipTree(configurations.monkeyPatchNonTransitive.singleFile)
+                        eachFile { FileCopyDetails action ->
+                            String path = action.relativePath.pathString
+                            def output = sourceSets.main.output
+                            boolean patched = new File(output.classesDir, path).exists() || new File(output.resourcesDir, path).exists()
+                            if (patched) {
+                                println "Using patched version of $path"
+                                action.exclude()
+                            }
+                        }
+                    }
+                }
+            }
+
+            initialized = true
+        }
+
+        boolean isInitialized() {
+            return initialized
+        }
+    }
+}
---
+++ spring-patch-consumer/src/test/java/com/sample/MonkeyPatchTest.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+import org.junit.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MonkeyPatchTest {
+	@Test
+	public void testMonkeyPat() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+    }
+}
